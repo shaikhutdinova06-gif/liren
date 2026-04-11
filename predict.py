4aa0b92fa5c60a00924ad8d0d508564e7d750528
shaikhutdinova06-gif <shaikhutdinova.06@gmail.com>
2026-04-11 11:03:21 +0500

    Simplify requirements and disable

---
+++ predict.py
@@ -0,0 +1,6 @@
+# Temporarily disabled for deployment - returns dummy data
+# TODO: Re-enable ML after deployment is working
+
+def predict(path):
+    # Dummy prediction for deployment
+    return "chernozem", 0.85
---
+++ soil_map_service.py
@@ -0,0 +1,6 @@
+# Temporarily disabled for deployment - returns dummy data
+# TODO: Re-enable raster after deployment is working
+
+def get_soil_type(lat, lon):
+    # Dummy soil type for deployment
+    return {"name": "chernozem"}
